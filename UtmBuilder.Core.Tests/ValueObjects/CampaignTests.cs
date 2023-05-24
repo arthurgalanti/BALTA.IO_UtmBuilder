01faa7410466c28d7af2ac01d06463edcf1091d0
ArthurGalanti <arthur.galanti@hotmail.com>
2023-05-24 02:40:53 -0300

    Adicionado Testes Campaign

---
+++ UtmBuilder.Core.Tests/ValueObjects/CampaignTests.cs
@@ -0,0 +1,40 @@
+﻿using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.Tests.ValueObjects;
+
+[TestClass]
+public class CampaignTests
+{
+    [TestMethod]
+    [DataRow("source", "medium", "campaign", false)]
+    [DataRow("source", "medium", "", true)]
+    [DataRow("source", "medium", null, true)]
+    [DataRow("source", "", "campaign", true)]
+    [DataRow("source", null, "campaign", true)]
+    [DataRow("", "medium", "campaign", true)]
+    [DataRow(null, "medium", "campaign", true)]
+    public void DadoUmaListaDeCampanhasDeveOuNaoLancarException(string source, string medium, string name, bool expectedExcepetion)
+    {
+        if (!expectedExcepetion)
+        {
+            new Campaign(source, medium, name);
+            Assert.IsTrue(true);
+        }
+        else
+        {
+            try
+            {
+                new Campaign(source, medium, name);
+                Assert.Fail();
+            }
+            catch (InvalidCampaignException)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+
+       
+        
+    }
+}
--- UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -6,15 +6,18 @@ namespace UtmBuilder.Core.Tests.ValueObjects;
 [TestClass]
 public class UrlTests
 {
+    private const string UrlInvalida = "galanti.dev";
+    private const string UrlValida = "https://galanti.dev";
+    
     [TestMethod]
-    public void DadoUmaUrlInvalidaDeveRetornaUmaException_TryCatch()
+    public void DadoUmaUrlInvalidaDeveRetornarUmaException_TryCatch()
     {
         try
         {
-            var url = new Url("galanti.dev");
+            var url = new Url(UrlInvalida);
             Assert.Fail();
         }
-        catch (InvalidUrlException e)
+        catch (InvalidUrlException)
         {
             Assert.IsTrue(true);
         }
@@ -22,15 +25,44 @@ public class UrlTests
     
     [TestMethod]
     [ExpectedException(typeof(InvalidUrlException))]
-    public void DadoUmaUrlInvalidaDeveRetornaUmaException_ExpextedException()
+    public void DadoUmaUrlInvalidaDeveRetornarUmaException_ExpextedException()
     {
-        var url = new Url("galanti.dev");
+        var url = new Url(UrlInvalida);
     }
     
     [TestMethod]
-    public void DadoUmaUrlValidaNaoDeveRetornaUmaException()
+    public void DadoUmaUrlValidaNaoDeveRetornarUmaException()
     {
-        var url = new Url("https://balta.io");
+        var url = new Url(UrlValida);
         Assert.IsTrue(true);
+    }
+    
+    [TestMethod]
+    [DataRow(" ",true)]
+    [DataRow("google",true)]
+    [DataRow("http",true)]
+    [DataRow(UrlValida,false)]
+    public void DadoUmaListaDeUrlsDeveRetornarOuNaoUmaException_DataRow(string link, bool exceptionEsperada)
+    {
+        if (!exceptionEsperada)
+        {
+            var url = new Url(link);
+            Assert.IsTrue(true);
+        }
+        else
+        {
+            try
+            {
+                var url = new Url(link);
+                Assert.Fail();
+            }
+            catch (InvalidUrlException)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+        
+        
+        
     }
 }
