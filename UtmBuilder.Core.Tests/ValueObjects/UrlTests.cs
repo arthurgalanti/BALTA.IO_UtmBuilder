cb4783e128a5306e0462513dae4855eb0d8a54d7
ArthurGalanti <arthur.galanti@hotmail.com>
2023-05-24 02:00:53 -0300

    Adicionado UrlExcepction e UrlTests

---
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -0,0 +1,36 @@
+﻿using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.Tests.ValueObjects;
+
+[TestClass]
+public class UrlTests
+{
+    [TestMethod]
+    public void DadoUmaUrlInvalidaDeveRetornaUmaException_TryCatch()
+    {
+        try
+        {
+            var url = new Url("galanti.dev");
+            Assert.Fail();
+        }
+        catch (InvalidUrlException e)
+        {
+            Assert.IsTrue(true);
+        }
+    }
+    
+    [TestMethod]
+    [ExpectedException(typeof(InvalidUrlException))]
+    public void DadoUmaUrlInvalidaDeveRetornaUmaException_ExpextedException()
+    {
+        var url = new Url("galanti.dev");
+    }
+    
+    [TestMethod]
+    public void DadoUmaUrlValidaNaoDeveRetornaUmaException()
+    {
+        var url = new Url("https://balta.io");
+        Assert.IsTrue(true);
+    }
+}
